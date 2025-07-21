cdb3173d88a293ce634eca9aac1fd81c82d74e74
Shahan pp <shahanmohmmed97@gmail.com>
2025-07-21 20:02:16 +0530

    changes

---
+++ src/pages/index.tsx
@@ -0,0 +1,54 @@
+import Head from "next/head";
+
+import { NextSeo } from "next-seo";
+
+import LandingHero from "@/components/landing-hero";
+import SkillsShowcase from "@/components/skills/skills-showcase";
+import ProjectShowcase from "@/components/projects/project-showcase";
+import { PROJECT_SHOWCASE } from "@/data/projects";
+import { SKILLS_DATA } from "@/data/skills";
+import { siteMetadata } from "@/data/siteMetaData.mjs";
+
+export default function Home() {
+  return (
+    <>
+      <NextSeo
+        title="Mohmmed Shahan | Software Developer"
+        description="Explore the professional portfolio of Mohmmed Shahan, a skilled Software Developer with 2 years of hands-on experience. Discover innovative projects, expertise in modern web technologies, and a passion for creating seamless user experiences."
+        canonical={siteMetadata.siteUrl}
+        openGraph={{
+          url: siteMetadata.siteUrl,
+          title: "Mohmmed Shahan - Software Developer",
+          description:
+            "Dive into the world of web development with Mohmmed Shahan. Discover a Software Developer with 2 years of expertise, showcasing cutting-edge projects and a commitment to crafting exceptional user interfaces.",
+          // images: [
+          //   {
+          //     url: `${siteMetadata.siteUrl}${siteMetadata.twitterImage}`,
+          //     alt: "Mohmmed Shahan - Portfolio Image",
+          //   },
+          // ],
+          siteName: siteMetadata.siteName,
+          type: "website",
+        }}
+        additionalMetaTags={[
+          {
+            property: "keywords",
+            content:
+              "React Developer, Software Developer, Frontend Developer, Web Developer, JavaScript, HTML, CSS, Portfolio, UI/UX, React.js, Frontend Development, Web Development, JavaScript Developer, Responsive Design",
+          },
+        ]}
+      />
+      <Head>
+        {siteMetadata.googleSiteVerification && (
+          <meta
+            name="google-site-verification"
+            content={siteMetadata.googleSiteVerification}
+          />
+        )}
+      </Head>
+      <LandingHero />
+      <SkillsShowcase skills={SKILLS_DATA} />
+      <ProjectShowcase projects={PROJECT_SHOWCASE} />
+    </>
+  );
+}
